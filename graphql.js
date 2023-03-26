941e7a1dd18cd5c63adfbaf413a42c2f1587af16
yangTauTauAjah <tesakun6464@gmail.com>
2023-03-26 21:18:53 +0700

    Added READ functionality

--- graphql.js
+++ graphql.js
@@ -1,32 +1,36 @@
-
-const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLID, GraphQLInt, GraphQLList } = require("graphql");
-const mockData = require('./mocked_datas/mock_book.json')
-
-
-
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLFloat,
+} = require("graphql");
+const mockData = require("./mocked_datas/mock_book.json");
+const mockUserData = require("./mocked_datas/mock_user_data.json");
+const mockFiles = require("./mocked_datas/mock_files.json");
+const mockComments = require("./mocked_datas/mock_comments.json");
 
 // READ
 
-
-
-const additionalData = new GraphQLObjectType({
-  name: 'AdditionalData',
+/* const additionalData = new GraphQLObjectType({
+  name: "AdditionalData",
   fields: {
     birthDate: {
-      type: GraphQLString
+      type: GraphQLString,
     },
     fullName: {
-      type: GraphQLString
+      type: GraphQLString,
     },
     nationality: {
-      type: GraphQLString
-    }
-  }
-})
-
+      type: GraphQLString,
+    },
+  },
+});
 
 const BookType = new GraphQLObjectType({
-  name: 'Book',
+  name: "Book",
   fields: () => {
     return {
       id: { type: GraphQLID },
@@ -36,42 +40,112 @@ const BookType = new GraphQLObjectType({
       additional: {
         type: additionalData,
         resolve: (obj, args, context, info) => {
-
-          console.log(context)
+          console.log(context);
 
           return {
             birthDate: new Date().toDateString(),
-            fullName: 'John Doe',
-            nationality: 'German'
-          }
-        }
-      }
-    }
-  }
-})
+            fullName: "John Doe",
+            nationality: "German",
+          };
+        },
+      },
+    };
+  },
+}); */
+
+const userType = new GraphQLObjectType({
+  name: "UserType",
+  fields: () => ({
+    id: { type: GraphQLID },
+    username: { type: GraphQLString },
+    email: { type: GraphQLString },
+    created_at: {
+      type: GraphQLString,
+      resolve(obj, arg) {
+        return new Date(Number.parseInt(obj.created_at)).toString();
+      },
+    },
+  }),
+});
+
+const fileType = new GraphQLObjectType({
+  name: "FileType",
+  fields: () => ({
+    id: { type: GraphQLID },
+    user_id: { type: GraphQLID },
+    username: {
+      type: GraphQLString,
+      resolve(obj) {
+        return mockUserData.find((e) => obj.user_id === e.id).username;
+      },
+    },
+    title: { type: GraphQLString },
+    tag: { type: GraphQLString },
+    file_size: { type: GraphQLFloat },
+    likes: { type: GraphQLInt },
+    dislikes: { type: GraphQLInt },
+    created_at: {
+      type: GraphQLString,
+      resolve(obj, arg) {
+        return new Date(Number.parseInt(obj.created_at)).toString();
+      },
+    },
+  }),
+});
+
+const commentType = new GraphQLObjectType({
+  name: "CommentType",
+  fields: () => ({
+    id: { type: GraphQLID },
+    user_id: { type: GraphQLID },
+    username: {
+      type: GraphQLString,
+      resolve(obj) {
+        return mockUserData.find((e) => obj.user_id === e.id).username;
+      },
+    },
+    comment: { type: GraphQLString },
+    likes: { type: GraphQLInt },
+    dislikes: { type: GraphQLInt },
+    created_at: {
+      type: GraphQLString,
+      resolve(obj, arg) {
+        return new Date(Number.parseInt(obj.created_at)).toString();
+      },
+    },
+  }),
+});
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: "RootQuery",
     fields: {
-      User: new GraphQLObjectType({
-        id,
-        username,
-        email,
-        createdAt
-      }),
-      Files: {
-        
+      User: {
+        type: new GraphQLList(userType),
+        resolve(obj, arg) {
+          return mockUserData;
+        },
       },
-      Post
-    }
+      Files: {
+        type: new GraphQLList(fileType),
+        resolve(obj, arg) {
+          return mockFiles;
+        },
+      },
+      Comments: {
+        type: new GraphQLList(commentType),
+        resolve(obj, arg) {
+          return mockComments;
+        },
+      },
+    },
   }),
   mutation: new GraphQLObjectType({
-    name: 'RootMutation',
+    name: "RootMutation",
     fields: {
-      name: { type: GraphQLString }
-    }
-  })
+      name: { type: GraphQLString },
+    },
+  }),
 });
 
-module.exports = {schema}
+module.exports = { schema };

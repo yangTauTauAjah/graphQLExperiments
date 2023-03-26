a1325beaed70f052cbf1c4a3449d45877e5ea462
yangTauTauAjah <tesakun6464@gmail.com>
2023-03-26 20:39:24 +0700

    mocking datas

--- graphql.js
+++ graphql.js
@@ -1,29 +1,76 @@
 
 const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLID, GraphQLInt, GraphQLList } = require("graphql");
-const mockData = require('./MOCK_DATA.json')
+const mockData = require('./mocked_datas/mock_book.json')
+
+
+
+
+// READ
+
+
+
+const additionalData = new GraphQLObjectType({
+  name: 'AdditionalData',
+  fields: {
+    birthDate: {
+      type: GraphQLString
+    },
+    fullName: {
+      type: GraphQLString
+    },
+    nationality: {
+      type: GraphQLString
+    }
+  }
+})
 
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
-  fields: () => ({
-    id: { type: GraphQLID },
-    title: { type: GraphQLString },
-    author: { type: GraphQLString },
-    pages: { type: GraphQLInt }
-  })
+  fields: () => {
+    return {
+      id: { type: GraphQLID },
+      title: { type: GraphQLString },
+      author: { type: GraphQLString },
+      pages: { type: GraphQLInt },
+      additional: {
+        type: additionalData,
+        resolve: (obj, args, context, info) => {
+
+          console.log(context)
+
+          return {
+            birthDate: new Date().toDateString(),
+            fullName: 'John Doe',
+            nationality: 'German'
+          }
+        }
+      }
+    }
+  }
 })
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: "RootQuery",
-    fields: () => ({
-      Book: {
-        type: new GraphQLList(BookType),
-        resolve: () => {
-          return mockData
-        },
+    fields: {
+      User: new GraphQLObjectType({
+        id,
+        username,
+        email,
+        createdAt
+      }),
+      Files: {
+        
       },
-    })
+      Post
+    }
+  }),
+  mutation: new GraphQLObjectType({
+    name: 'RootMutation',
+    fields: {
+      name: { type: GraphQLString }
+    }
   })
 });
 
